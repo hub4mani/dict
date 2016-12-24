d8dc56874f1a09261b27f6a3f44fc3b87c4db13e
hub4mani <video4mani@gmail.com>
2016-12-24 15:45:01 +0530

    Empty spring-boot application

---
+++ src/main/java/org/aadhavan/DictApplication.java
@@ -0,0 +1,12 @@
+package org.aadhavan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DictApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DictApplication.class, args);
+	}
+}
